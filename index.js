7fe8ce02673a9586992333f5b73407c6694e8a01
Igor Paraiso <igorparaisod@gmail.com>
2022-10-31 23:59:52 -0300

    Inicializando repo

---
+++ index.js
@@ -0,0 +1,34 @@
+const cores = ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","DarkOrange","DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed ","Indigo  ","Ivory","Khaki","Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan","LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon","LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow","Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple","RebeccaPurple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen","SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow","YellowGreen"]
+
+function escolheCor() {
+    return [cores[Math.floor(Math.random() * cores.length)]];
+}
+
+var color_options = [];
+
+var right_choice = escolheCor();
+color_options.push(right_choice);
+
+while (color_options.length < 10) {
+    let new_color = escolheCor();
+
+    if (color_options.indexOf(new_color) === -1) {
+        color_options.push(new_color);    
+    }   
+}
+
+function validateGuess(guess){
+    if (color_options.indexOf(guess) === -1){
+        alert("Ops, essa não é uma escolha válida");
+        start();
+    }
+}
+
+function start(){
+    let guess = prompt("Estou pensando em uma dessas cores, você consegue advinhar qual é?\n\n" + color_options);
+
+}
+
+color_options.sort();
+console.log(color_options);
+console.log(right_choice);
