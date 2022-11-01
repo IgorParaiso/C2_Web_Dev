10ab569c152a17a850b4d9995f5a87514e4c573d
Igor Paraiso <igorparaisod@gmail.com>
2022-11-01 12:50:18 -0300

    fix: na checagem do palpite

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const cores = ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","DarkOrange","DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed ","Indigo  ","Ivory","Khaki","Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan","LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon","LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow","Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple","RebeccaPurple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen","SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow","YellowGreen"]
+const cores = ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","DarkOrange","DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed ","Indigo  ","Ivory","Khaki","Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan","LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon" ,"LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow","Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple","RebeccaPurple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen","SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow","YellowGreen"]
 
 function escolheCor() {
-    return [cores[Math.floor(Math.random() * cores.length)]];
+    return cores[Math.floor(Math.random() * cores.length)];
 }
 
 var color_options = [];
@@ -18,11 +18,18 @@ while (color_options.length < 10) {
 }
 color_options.sort();
 
+function check(guess) {
+    if (color_options.includes(guess)){
+        return true;
+    }
+    return false;
+}
+
 function start(){
 
     while (true){
 
-        let guess = prompt("Estou pensando em uma dessas cores, você consegue advinhar qual é?\n\n" + color_options[0] + "\t" + color_options[1] + '\n' + color_options[2] + '\t' + color_options[3] + '\n' + color_options[4] + '\t' + color_options[5] + '\n' + color_options[6] + '\t' + color_options[7] + '\n' + color_options[8] + '\t' + color_options[9]);
+        let guess = prompt("Estou pensando em uma dessas cores, você consegue advinhar qual é?\n\n" + color_options[0] + "\t\t" + color_options[1] + '\n' + color_options[2] + '\t\t' + color_options[3] + '\n' + color_options[4] + '\t\t' + color_options[5] + '\n' + color_options[6] + '\t\t' + color_options[7] + '\n' + color_options[8] + '\t\t' + color_options[9]);
 
         if (guess == 'stop'){
             return false;
@@ -34,18 +41,13 @@ function start(){
             return true;
         }
         
-        if (color_options.indexOf(guess) == -1){
+        if (!check(guess)){
             alert("Ops, essa não é uma escolha válida");
         } else if (guess > right_choice) {
-            alert('Errou para mais');
+            alert('Errou!!! A cor que eu escolhi começa com uma letra menor');
         } else if (guess < right_choice){
-            alert('Errou para menos')
+            alert('Errou!!! A cor que eu escolhi começa com uma letra maior')
         }
-
-        
-
-        
-        
         
     }
     
